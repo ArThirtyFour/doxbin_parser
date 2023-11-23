e0a2c75c17aced4a5df840e4cd2bbe4e25f7150e
ArThirtyFour <149522975+ArThirtyFour@users.noreply.github.com>
2023-11-23 13:26:06 +0300

    Add file
    Da

---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import pip
+try:
+    import requests
+except ModuleNotFoundError:
+    print('Чел ты библотеку не поставил , я ее за тебя поставлю')
+    pip.main(['install','requests'])
+try:
+    from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+    print('Чел ты библотеку не поставил , я ее за тебя поставлю')
+    pip.main(['install','bs4'])
+try:
+    from art import tprint
+except ModuleNotFoundError:
+    print('Чел ты библотеку не поставил , я ее за тебя поставлю')
+    pip.main(['install','art'])
+
+
+def parser_deanona(url):
+    if requests.get(url).status_code == 404:
+        print('Чел такой пасты не существует')
+
+    else:
+        siilka = requests.get(url)
+        soup = BeautifulSoup(siilka.text,'html.parser')
+        deanon = soup.find_all('div',class_='show-container')
+        for text1 in deanon:
+            print(text1.text)
+
+
+
+if __name__ == '__main__':
+    tprint('''
+    DOXBIN PASTE 
+    PARSER''', font="small",space=1)
+    name_pasta=''
+    while True:
+        name_pasta=input('Введите название пасты которые вы хотите получить (пустота -  остановка цикла) : ').replace('_','')
+        if name_pasta == '':
+            print('Все я тебя понял , оставливаю процесс. ')
+            break
+        else:
+            parser_deanona(f'https://doxbin.org/upload/{name_pasta}')
